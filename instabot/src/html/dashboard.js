1041eaaf741b4262fcfa25887b3fae871d28d315
Odai Salem <odai.raed@gmail.com>
2023-10-26 23:27:08 +0300

    init dashboard

---
+++ instabot/src/html/dashboard.js
@@ -0,0 +1,32 @@
+function addNamesFromTextFile() {
+    const nameList = document.getElementById('name-list');
+
+    fetch('./followers.md')
+        .then(response => response.text())
+        .then(data => {
+            const names = data.split('\n');
+            const ul = document.createElement('ul');
+
+            names.forEach(function (name) {
+                const li = document.createElement('li');
+                li.textContent = name;
+                ul.appendChild(li);
+            });
+
+            nameList.innerHTML = '';
+            nameList.appendChild(ul);
+        })
+        .catch(error => {
+            console.error('Error fetching names.txt:', error);
+        });
+}
+
+
+function toggleSidebar() {
+    var sidebar = document.querySelector(".sidebar");
+    if (sidebar.classList.contains("sidebar-hidden")) {
+        sidebar.classList.remove("sidebar-hidden");
+    } else {
+        sidebar.classList.add("sidebar-hidden");
+    }
+}
---
+++ instabot/src/html/login.js
@@ -0,0 +1,20 @@
+function checkString() {
+    // Define the strings you want to check against
+    const definedStrings = ["odai.exe", "aquasazure", "iadox"];
+
+    // Get the input string from the user
+    const inputString = document.getElementById("inputString").value;
+    const messageElement = document.getElementById("message");
+
+    // Check if the input string is defined
+    if (definedStrings.includes(inputString)) {
+        // Redirect to fol.html if the string is defined
+        //window.location.href = "fol.html";
+        document.getElementById("message").innerHTML = "Your account is registered but the service is not yet ready on web.";
+        messageElement.style.color = "#02b4f5";
+    } else {
+        // Display an error message if the string is not defined
+        document.getElementById("message").innerHTML = "Your account is not registered. Please contact the administrator of instabot to register. (@odai.exe)";
+        messageElement.style.color = "#fa1302";
+    }
+}
