d407a360abb52a6311cf46fc8a583a71e8f7bfd1
odai.work4all <odai.raed@gmail.com>
2023-11-02 04:53:16 +0300

    forgot to code in a function

--- bot_backend/followers.py
+++ bot_backend/followers.py
@@ -1,21 +1,16 @@
 import instaloader
 
-L = instaloader.Instaloader()
+def get_followers(target):
+    L = instaloader.Instaloader()
 
-L.login("bot.wednesdayos","easytoguesspassword")
+    L.login("bot.wednesdayos","")
 
-target_account = 'odai.exe'
+    profile = instaloader.Profile.from_username(L.context, target)
 
-# Load the profile of the user
-profile = instaloader.Profile.from_username(L.context, target_account)
+    followers = []
 
-# Create an empty list to store the followers
-followers = []
+    for follower in profile.get_followers():
+        followers.append(follower.username)
 
-# Iterate through the followers and add them to the list
-for follower in profile.get_followers():
-    followers.append(follower.username)
-
-# Print the list of followers
-for follower_username in followers:
-    print(follower_username)
+    for follower_username in followers:
+        print(follower_username)
